c24f8a9b8f7007a0016275a73cdb18f7b67f205c
bhaviljain <bhaviljain26@gmail.com>
2024-06-02 15:27:19 +0530

    Carousel

--- src/ImageSlider/Carousels.jsx
+++ src/ImageSlider/Carousels.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
+//used the react multi carousel library to make the website look as real as possible
 
 const Carousels = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
+      // 
       breakpoint: { max: 4000, min: 1024 },
-      items: 5
+      items: 5 //this denotes how many items can be seen on UI
     },
     desktop: {
       breakpoint: { max: 1024, min: 800 },
@@ -20,17 +21,17 @@ const Carousels = () => {
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
+      items: 2
     }
   };
-  
+    
+  //state for storing the data 
     const [ResData, setResData] = useState([])
 
     const fetchRest = async () =>
         {
           const data = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
           const res =  await data.json()
-          // console.log(res);
           setResData(res.meals)
         }
         useEffect(()=>{
@@ -44,12 +45,12 @@ const Carousels = () => {
     <Carousel responsive={responsive}
     className='shadow-lg onplusnord3:pl-36 iPhoneSE:pl-2'
     >
-   {ResData.map((items)=>(
-    <div className='mt-28 bg-white '>
+   {ResData.map((items,idMeal)=>(
+    <div key={idMeal} className='mt-28 bg-white '>
    <img src={items.strMealThumb}
-   className='h-40 w-40  rounded-full onplusnord3:h-28 onplusnord3:w-28 iPhoneSE:h-28 iPhoneSE:w-28 SamsungS8:h-20 SamsungS8:w-20 SamsungS8:ml-36 zfold:h-20 zfold:w-20 zfold:ml-32'
+   className='h-40 w-40 rounded-full onplusnord3:h-28 onplusnord3:w-28 iPhoneSE:h-24 iPhoneSE:w-28 SamsungS8:h-20 SamsungS8:w-20 SamsungS8:ml-36 zfold:h-20 zfold:w-20 zfold:ml-12'
    />
-   <h4 className='font-bold text-center onplusnord3:pr-12 pt-5 iPhoneSE:pr-30'>{items.strMeal}</h4>
+   <h4 className='font-bold text-center onplusnord3:pr-12 pt-5'>{items.strMeal}</h4>
    </div>
    ))}
   
